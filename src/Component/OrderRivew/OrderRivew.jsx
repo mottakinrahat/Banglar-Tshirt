80601bd7fda299443d66807060c857c9549a06bf
mottakinrahat <rahatchowhdury661@gmail.com>
2023-04-09 23:09:20 +0600

    initialCommit

---
+++ src/Component/OrderRivew/OrderRivew.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const OrderRivew = () => {
+    return (
+        <div>
+            <h1>Order rivew page</h1>
+        </div>
+    );
+};
+
+export default OrderRivew;
---
+++ src/main.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Home from './Component/Home/Home';
+import Main from './Component/Layout/Main';
+import OrderRivew from './Component/OrderRivew/OrderRivew';
+
+const router=createBrowserRouter([
+  {
+    path:'/',
+    element:<Main/>,
+    children:[
+      {
+        path:'/',
+        element:<Home/>,
+        loader:()=>fetch('tshirts.json')
+      },
+      {
+        path:'review',
+        element:<OrderRivew/>
+      }
+    ]
+  }
+])
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+      <RouterProvider router={router} />
+  </React.StrictMode>,
+)
